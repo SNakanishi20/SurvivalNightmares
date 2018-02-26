5dc57ac54965432083831ff5a1bb8f8ebb7590c6
SNakanishi20 <S.Nakanishi20@gmail.com>
2018-02-26 18:14:57 +0900

    Unity Project Created.

---
+++ SurvivalNightmares/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5.0f;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		//オフセットを設定(基準が存在する地点)
+		offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector3 targetCamPos = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing);
+	}
+}
